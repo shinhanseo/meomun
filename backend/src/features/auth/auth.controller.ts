19b7c48f7d018149347d1278720ef21c8869dee8
Shin Han Seo <imkara1@naver.com>
2026-06-15 11:07:27 +0900

    Feat : authController 코드 작성

--- backend/src/features/auth/auth.controller.ts
+++ backend/src/features/auth/auth.controller.ts
@@ -1,1 +1,27 @@
-export class AuthController {}
+import type { Request, Response } from 'express';
+
+import { AuthService } from './auth.service.js';
+import type { KakaoLoginRequest } from './auth.types.js';
+
+export class AuthController {
+  constructor(private readonly authService = new AuthService()) { }
+
+  loginWithKakao = async (
+    request: Request<object, object, KakaoLoginRequest>,
+    response: Response
+  ) => {
+    const { kakaoAccessToken } = request.body
+
+    if (!kakaoAccessToken) {
+      response.status(400).json({
+        message: 'kakaoAccessToken이 필요합니다.',
+      });
+
+      return;
+    }
+
+    const result = await this.authService.loginWithKakao(kakaoAccessToken);
+
+    response.status(200).json(result);
+  }
+}
