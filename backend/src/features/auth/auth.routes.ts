5f233eec1c781e723157169b14cd9d04094bf65f
Shin Han Seo <imkara1@naver.com>
2026-06-15 11:08:52 +0900

    Feat : loginWithKakao api 연결

--- backend/src/features/auth/auth.routes.ts
+++ backend/src/features/auth/auth.routes.ts
@@ -1,5 +1,9 @@
 import { Router } from 'express';
+import { AuthController } from './auth.controller.js';
 
 const authRoutes = Router();
+const authController = new AuthController();
+
+authRoutes.post('/kakao', authController.loginWithKakao);
 
 export default authRoutes;
