c79b34631ebba929fa78cc22276848a990611dbc
Shin Han Seo <imkara1@naver.com>
2026-06-15 10:36:10 +0900

    Feat : 기존 유저 조회 및 신규 유저 생성 method 추가

--- backend/src/features/auth/auth.repository.ts
+++ backend/src/features/auth/auth.repository.ts
@@ -1,1 +1,45 @@
-export class AuthRepository {}
+import { AuthProvider } from '../../generated/prisma/enums.js';
+import { database } from '../../db.js';
+
+export class AuthRepository {
+  /* 
+    기존에 가입한 유저인지 확인하는 메서드 
+    True : user 반환, False : null 반환
+  */
+  findOauthAccount(provider: AuthProvider, providerUserId: string) {
+    return database.oAuthAccount.findUnique({
+      where: {
+        provider_providerUserId: {
+          provider,
+          providerUserId,
+        },
+      },
+      include: {
+        user: true,
+      }
+    });
+  }
+
+  createUserWithOauthAccount(
+    provider: AuthProvider,
+    providerUserId: string,
+    email?: string,
+    nickname?: string,
+  ) {
+    return database.user.create({
+      data: {
+        nickname,
+        oauthAccounts: {
+          create: {
+            provider,
+            providerUserId,
+            email,
+          },
+        },
+      },
+      include: {
+        oauthAccounts: true,
+      }
+    });
+  }
+}
