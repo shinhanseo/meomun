4604d2b11c75e34e14098ea9c5714415f27189a8
Shin Han Seo <imkara1@naver.com>
2026-06-15 10:54:44 +0900

    Feat : Kakao Oauth 연결

--- backend/src/features/auth/auth.service.ts
+++ backend/src/features/auth/auth.service.ts
@@ -1,1 +1,31 @@
-export class AuthService {}
+import { AuthProvider } from '../../generated/prisma/enums.js';
+import { AuthRepository } from './auth.repository.js';
+import { KakaoProvider } from './providers/kakao.provider.js';
+
+export class AuthService {
+  constructor(
+    private readonly authRepository = new AuthRepository(),
+    private readonly kakaoProvider = new KakaoProvider(),
+  ) { }
+
+  async loginWithKakao(kakaoAccessToken: string) {
+    const kakaoProfile = await this.kakaoProvider.getProfile(kakaoAccessToken);
+
+    const oauthAccount = await this.authRepository.findOauthAccount(
+      AuthProvider.KAKAO,
+      kakaoProfile.providerUserId,
+    );
+
+    if (oauthAccount) {
+      return oauthAccount.user;
+    }
+
+    return this.authRepository.createUserWithOauthAccount(
+      AuthProvider.KAKAO,
+      kakaoProfile.providerUserId,
+      kakaoProfile.email,
+      kakaoProfile.nickname,
+    );
+
+  }
+}
