daadb1e849ef07219cd1bdab3f64be9426ea1100
Shin Han Seo <imkara1@naver.com>
2026-06-17 01:31:56 +0900

    Feat : User Repository.ts 추가

--- backend/src/features/users/users.repository.ts
+++ backend/src/features/users/users.repository.ts
@@ -1,1 +1,22 @@
-export class UsersRepository {}
+import { database } from '../../db.js';
+
+export class UsersRepository {
+  findById(userId: string) {
+    return database.user.findUnique({
+      where: {
+        id: userId,
+      }
+    });
+  }
+
+  updateById(userId: string, nickname: string | null) {
+    return database.user.update({
+      where: {
+        id: userId,
+      },
+      data: {
+        nickname,
+      }
+    });
+  }
+}
