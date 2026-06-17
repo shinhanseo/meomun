144b54e2f0b0d5caa495280020d907afff7bd0b6
Shin Han Seo <imkara1@naver.com>
2026-06-17 23:27:42 +0900

    Feat : QueryProvider.tsx 추가

---
+++ frontend/src/app/providers/QueryProvider.tsx
@@ -0,0 +1,20 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { useState, type PropsWithChildren } from 'react';
+
+export function QueryProvider({ children }: PropsWithChildren) {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            retry: 1,
+            staleTime: 1000 * 30,
+          },
+        },
+      }),
+  );
+
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+}
