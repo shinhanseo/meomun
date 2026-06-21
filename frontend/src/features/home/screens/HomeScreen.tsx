5321439cbdb42605d6b0bf0ff2f498efa512bd74
Shin Han Seo <imkara1@naver.com>
2026-06-21 15:36:41 +0900

    Feat : Naver Map 컴포넌트 추가

--- frontend/src/features/home/screens/HomeScreen.tsx
+++ frontend/src/features/home/screens/HomeScreen.tsx
@@ -1,25 +1,17 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
-import { semanticColor } from '../../../shared/constants/color';
+import { HomeMap } from '../components/HomeMap';
 
 export function HomeScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>지도</Text>
+      <HomeMap />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    alignItems: 'center',
-    backgroundColor: semanticColor.background,
     flex: 1,
-    justifyContent: 'center',
-  },
-  title: {
-    color: semanticColor.textPrimary,
-    fontSize: 28,
-    fontWeight: '700',
   },
 });
