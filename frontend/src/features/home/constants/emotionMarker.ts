3d78d6c7794f8ae549869a287dc7ed16534df20d
Shin Han Seo <imkara1@naver.com>
2026-06-22 12:38:25 +0900

    Feat : 감정별 마커 이미지 연결

--- frontend/src/features/home/constants/emotionMarker.ts
+++ frontend/src/features/home/constants/emotionMarker.ts
@@ -2,35 +2,35 @@ import { color } from '../../../shared/constants/color';
 
 export const emotionMarkerMeta = {
   HAPPY: {
-    image: require('../../../assets/emotions/happy.png'),
+    image: require('../../../assets/markers/marker_HAPPY.png'),
     color: '#F08BA8',
   },
   SAD: {
-    image: require('../../../assets/emotions/sad.png'),
+    image: require('../../../assets/markers/marker_SAD.png'),
     color: '#7EA4DF',
   },
   ANGRY: {
-    image: require('../../../assets/emotions/angry.png'),
+    image: require('../../../assets/markers/marker_ANGRY.png'),
     color: '#E85D75',
   },
   ANXIOUS: {
-    image: require('../../../assets/emotions/anxious.png'),
+    image: require('../../../assets/markers/marker_ANXIOUS.png'),
     color: '#F5C451',
   },
   CALM: {
-    image: require('../../../assets/emotions/calm.png'),
+    image: require('../../../assets/markers/marker_CALM.png'),
     color: color.purple[500],
   },
   FLUTTER: {
-    image: require('../../../assets/emotions/flutter.png'),
+    image: require('../../../assets/markers/marker_FLUTTER.png'),
     color: '#DE73A7',
   },
   REFLECTIVE: {
-    image: require('../../../assets/emotions/reflective.png'),
+    image: require('../../../assets/markers/marker_REFLECTIVE.png'),
     color: '#68B987',
   },
   TIRED: {
-    image: require('../../../assets/emotions/tired.png'),
+    image: require('../../../assets/markers/marker_TIRED.png'),
     color: '#9A94AA',
   },
 } as const;
