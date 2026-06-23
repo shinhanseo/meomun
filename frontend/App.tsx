d0794d345ba2d522201374729ac9301ca46632ed
Shin Han Seo <imkara1@naver.com>
2026-06-23 16:21:04 +0900

    Chore : 바텀시트 라이브러리 설치

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,13 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 import { RootNavigator } from './src/app/navigation/RootNavigator';
 import { AppProvider } from './src/app/providers/AppProvider';
 
 export default function App() {
   return (
-    <AppProvider>
-      <RootNavigator />
-      <StatusBar style="dark" />
-    </AppProvider>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <AppProvider>
+        <RootNavigator />
+        <StatusBar style="dark" />
+      </AppProvider>
+    </GestureHandlerRootView>
   );
 }
