f1b469ac7d925258736df47a778429ad91998dd0
Shin Han Seo <imkara1@naver.com>
2026-06-23 14:29:28 +0900

    Fix : api endpoint 오타 수정

--- frontend/src/features/record/api/recordApi.ts
+++ frontend/src/features/record/api/recordApi.ts
@@ -4,7 +4,7 @@ import type { RecordResponse } from "../types/record.types";
 export const recordApi = {
   async getRecordDetail(recordId: string) {
     const { data } = await apiClient.get<RecordResponse>(
-      `api/records/${recordId}`,
+      `/api/records/${recordId}`,
     );
 
     return data;
