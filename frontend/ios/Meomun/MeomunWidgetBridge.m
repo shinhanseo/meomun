66a38c46538b50b290f5eea967f72240cfade56b
Shin Han Seo <imkara1@naver.com>
2026-07-02 14:45:26 +0900

    Feat: 앱, 위젯 AppGroup 연결

---
+++ frontend/ios/Meomun/MeomunWidgetBridge.m
@@ -0,0 +1,34 @@
+#import <Foundation/Foundation.h>
+#import <React/RCTBridgeModule.h>
+
+static NSString *const MeomunWidgetAppGroup = @"group.com.hanseo.meomun";
+static NSString *const MeomunTodayWidgetSummaryKey = @"today_widget_summary";
+
+@interface MeomunWidgetBridge : NSObject <RCTBridgeModule>
+@end
+
+@implementation MeomunWidgetBridge
+
+RCT_EXPORT_MODULE();
+
+RCT_EXPORT_METHOD(saveTodayWidgetSummary
+                  : (NSString *)summaryJson resolver
+                  : (RCTPromiseResolveBlock)resolve rejecter
+                  : (RCTPromiseRejectBlock)reject)
+{
+  NSUserDefaults *defaults =
+      [[NSUserDefaults alloc] initWithSuiteName:MeomunWidgetAppGroup];
+
+  if (defaults == nil) {
+    reject(@"E_WIDGET_APP_GROUP_UNAVAILABLE",
+           @"Unable to access the Meomun App Group user defaults.", nil);
+    return;
+  }
+
+  [defaults setObject:summaryJson forKey:MeomunTodayWidgetSummaryKey];
+  [defaults synchronize];
+
+  resolve(nil);
+}
+
+@end
---
+++ frontend/ios/MeomunWidget/MeomunWidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  MeomunWidgetBundle.swift
+//  MeomunWidget
+//
+//  Created by Shin Han Seo on 7/2/26.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct MeomunWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        MeomunWidget()
+        MeomunWidgetControl()
+    }
+}
